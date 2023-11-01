31f2956e47ec8b91884da8f83296114438e11fd3
Mohammed Amine <aminebenmade1999@gmial.com>
2023-11-01 12:12:02 +0100

    last commit before js

--- scripts/main.js
+++ scripts/main.js
@@ -38,15 +38,15 @@ loadWeeksTd();
 
 // Get Start & End point for table tr :
 function getStartPoint(d, m, y) {
-  const startDate = new Date(y, m - 1, d);
-  const dayNumber = startDate.getDay();
-  return dayNumber == 1 ? startDate : new Date(y, m - 1, d - dayNumber + 1);
+    const startDate = new Date(y, m - 1, d);
+    const dayNumber = startDate.getDay();
+    return dayNumber == 1 ? startDate : new Date(y, m - 1, d - dayNumber + 1);
 }
 
 function getEndPoint(d, m, y) {
-  const endDate = new Date(y, m - 1, d);
-  const dayNumber = endDate.getDay();
-  return dayNumber == 0 ? endDate : new Date(y, m - 1, d + 7 - dayNumber);
+    const endDate = new Date(y, m - 1, d);
+    const dayNumber = endDate.getDay();
+    return dayNumber == 0 ? endDate : new Date(y, m - 1, d + 7 - dayNumber);
 }
 
 // load table td for months :
@@ -54,34 +54,48 @@ function loadMonthsTd() {}
 
 // load table td for weekdays :
 function loadWeeksTd(startDate, endDate) {
-  const startDate = getStartPoint(21, 10, 2023);
-  const endDate = getEndPoint(9, 12, 2023);
+    const startDate = getStartPoint(21, 10, 2023);
+    const endDate = getEndPoint(9, 12, 2023);
   const currDate = startDate;
 
   let i = 0;
   while (currDate.getTime() !== endDate.getTime()) {
-    if (i == 100) break;
-
-    weekdaysTr.insertAdjacentHTML(
-      "beforeend",
-      `<td>${currDate.getDate()}</td>`
+      if (i == 100) break;
+      
+      weekdaysTr.insertAdjacentHTML(
+          "beforeend",
+          `<td>${currDate.getDate()}</td>`
     );
     currDate.setDate(currDate.getDate() + 1);
     i++;
-  }
+}
 }
 
 // change vue :
 const controlVue = (e) => {
-  const value = e.target.textContent.toLowerCase();
-
-  Object.values(e.currentTarget.children).forEach((item) =>
+    const value = e.target.textContent.toLowerCase();
+    
+    Object.values(e.currentTarget.children).forEach((item) =>
     item.classList.remove("active")
-  );
-  e.target.classList.add("active");
-  if (value == "months") {
-    weekdaysTr.classList.add("hidden");
-  } else if (value == "weeks") {
-    weekdaysTr.classList.remove("hidden");
-  }
+    );
+    e.target.classList.add("active");
+    if (value == "months") {
+        weekdaysTr.classList.add("hidden");
+    } else if (value == "weeks") {
+        weekdaysTr.classList.remove("hidden");
+    }
 };
+
+/***  settings PAGE STYLES    ***/
+/***  settings PAGE STYLES    ***/
+
+/***  INFO_SECTION STYLES    ***/
+
+
+
+
+
+
+/***  USER_SECTION STYLES    ***/
+
+
