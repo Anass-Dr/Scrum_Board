d1a32b35951a3826ba8b7d6de01053c86a327dc7
ait ouaguerd anass <aitougeurdanass@gmail.com>
2023-11-01 23:07:42 +0100

    add dragable and drop the users stories

--- scripts/main.js
+++ scripts/main.js
@@ -3,39 +3,37 @@
 let draggedElement = null;
 
 function start(e) {
-    e.dataTransfer.effectAllowed = "move";
-    e.dataTransfer.setData("text", e.target.id);
-    draggedElement = e.target;
+  e.dataTransfer.effectAllowed = "move";
+  e.dataTransfer.setData("text", e.target.id);
+  draggedElement = e.target;
 }
 
 function over(e) {
-    e.preventDefault();
+  e.preventDefault();
 }
 
 function drop(e) {
-    e.preventDefault();
-    var id = e.dataTransfer.getData("text");
-    var element = document.getElementById(id);
-    var enCoursZone = e.currentTarget; // La zone "En Cours"
+  e.preventDefault();
+  var id = e.dataTransfer.getData("text");
+  var element = document.getElementById(id);
+  var enCoursZone = e.currentTarget; // La zone "En Cours"
 
-    
-    const mouseY = e.clientY;
-    
-    let targetElement = null; 
-    for (let child of enCoursZone.children) {
-        const rect = child.getBoundingClientRect();
-        if (mouseY < rect.top + rect.height / 2) {
-            targetElement = child;
-            break;
-        }
+  const mouseY = e.clientY;
+
+  let targetElement = null;
+  for (let child of enCoursZone.children) {
+    const rect = child.getBoundingClientRect();
+    if (mouseY < rect.top + rect.height / 2) {
+      targetElement = child;
+      break;
     }
+  }
 
+  if (targetElement) {
+    enCoursZone.insertBefore(element, targetElement);
+  } else {
+    enCoursZone.appendChild(element);
+  }
 
-    if (targetElement) {
-        enCoursZone.insertBefore(element, targetElement);
-    } else {
-        enCoursZone.appendChild(element);
-    }
-
-    draggedElement = null;
+  draggedElement = null;
 }
