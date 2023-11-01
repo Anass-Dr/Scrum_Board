d8a7a0cfaba7d93536c3c0294e3978fa8d2856d5
unknown <chakirlatifa2001@gmail.com>
2023-11-01 09:01:42 +0100

    position of userStory in drag and drop

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
 "use strict";
 
+let draggedElement = null;
+
 function start(e) {
     e.dataTransfer.effectAllowed = "move";
     e.dataTransfer.setData("text", e.target.id);
+    draggedElement = e.target;
 }
 
 function over(e) {
@@ -13,5 +16,26 @@ function drop(e) {
     e.preventDefault();
     var id = e.dataTransfer.getData("text");
     var element = document.getElementById(id);
-    e.currentTarget.appendChild(element);
+    var enCoursZone = e.currentTarget; // La zone "En Cours"
+
+    
+    const mouseY = e.clientY;
+    
+    let targetElement = null; 
+    for (let child of enCoursZone.children) {
+        const rect = child.getBoundingClientRect();
+        if (mouseY < rect.top + rect.height / 2) {
+            targetElement = child;
+            break;
+        }
+    }
+
+
+    if (targetElement) {
+        enCoursZone.insertBefore(element, targetElement);
+    } else {
+        enCoursZone.appendChild(element);
+    }
+
+    draggedElement = null;
 }
