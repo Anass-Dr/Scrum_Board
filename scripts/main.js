2c24401bb47b1605f9313bf6eb702ad89ff38f38
Anass-Dr <anasdrissi77@gmail.com>
2023-11-01 23:12:38 +0100

    dynamic timeline added for weeks vue

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,28 @@
 // GLOBAL VARIABLES :
 const monthsTr = document.getElementById("months");
 const weekdaysTr = document.getElementById("weekdays");
+const sprintsTr = document.getElementById("sprints-tr");
+const tbody = document.querySelector("tbody");
+const timelineMonths = [
+  "JAN",
+  "FEB",
+  "MAR",
+  "APR",
+  "MAY",
+  "JUN",
+  "JUL",
+  "AUG",
+  "SEP",
+  "OCT",
+  "NOV",
+  "DEC",
+];
 
 const sprints = [
   {
     name: "Sprint 1",
     duration: 7,
-    start: "2023-10-31",
+    start: "2023-10-21",
     user_stories: [
       {
         name: "User Story 1",
@@ -18,7 +34,7 @@ const sprints = [
       {
         name: "User Story 2",
         status: "To Do",
-        duration: 3,
+        duration: 1,
       },
       {
         name: "User Story 3",
@@ -30,11 +46,23 @@ const sprints = [
         status: "To Do",
         duration: 1,
       },
+      {
+        name: "User Story 5",
+        status: "To Do",
+        duration: 2,
+        user_stories: [],
+      },
     ],
   },
+  {
+    name: "Sprint 2",
+    duration: 8,
+    start: "2023-11-1",
+  },
 ];
-loadWeeksTd();
+
 /***  TIMELINE PAGE STYLES    ***/
+loadTableRows();
 
 // Get Start & End point for table tr :
 function getStartPoint(d, m, y) {
@@ -49,15 +77,50 @@ function getEndPoint(d, m, y) {
   return dayNumber == 0 ? endDate : new Date(y, m - 1, d + 7 - dayNumber);
 }
 
+// Get Month for each table td :
+function getMonth(startDate) {
+  const currDate = new Date(startDate.getTime());
+  const firstMonth = startDate.getMonth();
+
+  let firstMonthCount = 0;
+  let secondMonthCount = 0;
+  for (let i = 0; i < 7; i++) {
+    currDate.getMonth() == firstMonth ? firstMonthCount++ : secondMonthCount++;
+    currDate.setDate(currDate.getDate() + 1);
+  }
+
+  return firstMonthCount > secondMonthCount
+    ? firstMonth - 1
+    : currDate.getMonth() - 1;
+}
+
+// Load table rows :
+function loadTableRows() {
+  const startDate = getStartPoint(21, 10, 2023);
+  const endDate = getEndPoint(9, 12, 2023);
+  loadWeeksTd(startDate, endDate);
+  loadMonthsTd(startDate, endDate);
+  loadSprints(startDate);
+  loadUserStories(startDate);
+}
+
 // load table td for months :
-function loadMonthsTd() {}
+function loadMonthsTd(startDate, endDate) {
+  const monthsNumber = (endDate - startDate) / 86400000 / 7;
+  const currDate = new Date(startDate.getTime());
+
+  for (let i = 0; i < monthsNumber; i++) {
+    monthsTr.insertAdjacentHTML(
+      "beforeend",
+      `<th scope="col" colspan="7">${timelineMonths[getMonth(currDate)]}</th>`
+    );
+    currDate.setDate(currDate.getDate() + 7);
+  }
+}
 
 // load table td for weekdays :
 function loadWeeksTd(startDate, endDate) {
-  const startDate = getStartPoint(21, 10, 2023);
-  const endDate = getEndPoint(9, 12, 2023);
-  const currDate = startDate;
-
+  const currDate = new Date(startDate.getTime());
   let i = 0;
   while (currDate.getTime() !== endDate.getTime()) {
     if (i == 100) break;
@@ -69,6 +132,54 @@ function loadWeeksTd(startDate, endDate) {
     currDate.setDate(currDate.getDate() + 1);
     i++;
   }
+}
+
+// load Sprints in table :
+function loadSprints(startDate) {
+  sprints.forEach((sprint, indx) => {
+    let colspan;
+    if (indx == 0) colspan = (new Date(sprint.start) - startDate) / 86400000;
+    else {
+      const endDate = new Date(sprints[indx - 1].start);
+      endDate.setDate(endDate.getDate() + sprints[indx - 1].duration);
+      colspan = (new Date(sprint.start) - endDate) / 86400000;
+    }
+
+    sprintsTr.insertAdjacentHTML(
+      "beforeend",
+      `
+        <td colspan="${colspan}"></td>
+        <td colspan="${sprint.duration}" class="sprints">
+          <span class="d-block bg-sprint1 rounded-3 fs-14 text-primary">
+            ${sprint.name}
+          </span>
+        </td>
+        `
+    );
+  });
+}
+
+// load User Stories in Table :
+function loadUserStories(startDate) {
+  sprints.forEach((sprint) => {
+    const sprintStart = new Date(sprint.start);
+    sprint.user_stories?.forEach((story) => {
+      const colspan = (sprintStart - startDate) / 86400000;
+      tbody.insertAdjacentHTML(
+        "beforeend",
+        `
+        <tr>
+          <td colspan="${colspan}"></td>
+          <td style="--i: 1" class="user_story user1" colspan="${story.duration}">
+            <span></span>
+          </td>
+        </tr>
+      `
+      );
+
+      sprintStart.setDate(sprintStart.getDate() + story.duration);
+    });
+  });
 }
 
 // change vue :
