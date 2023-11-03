859bf9f055b73900b743e094be72301f8e7eb198
unknown <chakirlatifa2001@gmail.com>
2023-11-03 10:04:17 +0100

    adding delete and edit user story

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,12 @@ function start(e) {
     e.dataTransfer.effectAllowed = "move";
     e.dataTransfer.setData("text", e.target.id);
     draggedElement = e.target;
-  
+
 }
 
 function over(e) {
     e.preventDefault();
-   
+
 }
 
 function drop(e) {
@@ -20,10 +20,10 @@ function drop(e) {
     var element = document.getElementById(id);
     var enCoursZone = e.currentTarget; // La zone "En Cours"
 
-    
+
     const mouseY = e.clientY;
-    
-    let targetElement = null; 
+
+    let targetElement = null;
     for (let child of enCoursZone.children) {
         const rect = child.getBoundingClientRect();
         if (mouseY < rect.top + rect.height / 2) {
@@ -40,4 +40,43 @@ function drop(e) {
     }
 
     draggedElement = null;
+}
+
+// Changer la taille du texte
+function changeFontSize() {
+    var fontSize = document.getElementById("fontSize").value;
+    document.getElementById("textArea").style.fontSize = fontSize;
+}
+
+// Changer la couleur du texte
+function changeFontColor() {
+    var fontColor = document.getElementById("fontColor").value;
+    document.getElementById("textArea").style.color = fontColor;
+}
+
+
+ // Sélectionnez tous les boutons par leur classe
+ var modifierButtons = document.querySelectorAll(".modifierButton");
+
+ // Parcourez les boutons et ajoutez un événements "click"
+ modifierButtons.forEach(function (button) {
+     button.addEventListener("click", function () {
+         // Trouvez le div parent pour permettre la modification
+         var parentDiv = button.previousElementSibling;
+         parentDiv.focus();
+     });
+ });
+
+
+
+ function editParagraph(button) {
+    var parentParagraph = button.previousElementSibling;
+    parentParagraph.focus();
+}
+
+function supprimerStory(button) {
+    var userStory = button.closest('.zone');
+    if (userStory) {
+        userStory.remove();
+    }
 }
